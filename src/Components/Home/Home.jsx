9d06430955290b520eaf55adbce8f2e264d4fbc7
Jackson <z5397791@ad.unsw.edu.au>
2023-01-31 19:20:00 +1100

    Add home footer icon with react icons

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./home.css";
 import video from "../../assets/video.mp4";
 import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { FaTripadvisor } from "react-icons/fa";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { BsListTask } from "react-icons/bs";
+import { TbApps } from "react-icons/tb";
 
 const Home = () => {
   return (
@@ -37,6 +43,23 @@ const Home = () => {
             <div className="input flex">
               <input type="range" max="5000" min="1000" />
             </div>
+          </div>
+
+          <div className="searchOptions flex">
+            <HiFilter className="icon" />
+            <span>More filters</span>
+          </div>
+        </div>
+
+        <div className="homeFooterIcons flex">
+          <div className="rightIcons">
+            <FiFacebook className="icon"/>
+            <AiOutlineInstagram className="icon"/>
+            <FaTripadvisor className="icon"/>
+          </div>
+          <div className="leftIcons">
+            <BsListTask className="icon" />
+            <TbApps className="icon" />
           </div>
         </div>
       </div>
