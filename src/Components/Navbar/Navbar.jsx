025166e80585fe6f9aad1a8d2c077dfad61cae72
Jackson <z5397791@ad.unsw.edu.au>
2023-01-31 17:47:42 +1100

    Add navbar toggle and close function to navbar component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import { MdTravelExplore } from "react-icons/md";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { TbGridDots } from "react-icons/tb";
 
 const Navbar = () => {
+  const [active, setActive] = useState('navBar');
+
+  // Toggle navBar
+  const showNav = () => {
+    setActive('navBar activeNavbar');
+  }
+
+  // Close navBar
+  const removeNavbar = () => {
+    setActive('navBar');
+  }
+
   return (
     <section className="navBarSection">
       <header className="header flex">
@@ -16,7 +28,7 @@ const Navbar = () => {
           </a>
         </div>
 
-        <div className="navBar">
+        <div className={active}>
           <ul className="navLists flex">
             <li className="navItem">
               <a href="#" className="navLink">Home</a>
@@ -45,12 +57,12 @@ const Navbar = () => {
             </button>
           </ul>
 
-          <div className="closeNavbar">
+          <div onClick={removeNavbar} className="closeNavbar">
             <AiFillCloseCircle className="icon"/>
           </div>
         </div>
 
-        <div className="toggleNavbar">
+        <div onClick={showNav} className="toggleNavbar">
           <TbGridDots className="icon"/>
         </div>
         
