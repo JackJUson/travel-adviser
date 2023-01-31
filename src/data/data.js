901cc8723d9400aa75dd3183714c78f0e53979c5
Jackson <z5397791@ad.unsw.edu.au>
2023-01-31 21:30:54 +1100

    Add holiday destination data array in data.js

---
+++ src/data/data.js
@@ -0,0 +1,102 @@
+import img from "../assets/img.png";
+import img2 from "../assets/img2.png";
+import img3 from "../assets/img3.png";
+import img4 from "../assets/img4.png";
+import img5 from "../assets/img5.png";
+import img6 from "../assets/img6.png";
+import img7 from "../assets/img7.png";
+import img8 from "../assets/img8.png";
+import img9 from "../assets/img9.png";
+
+export const holidayDestinations = [
+  {
+    id: 1,
+    imgSrc: img,
+    destTitle: "Bora Bora",
+    location: "New Zealand",
+    grade: "CULTURAL RELAX",
+    fees: "$800",
+    description:
+      "The epitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxurious stays and adventurous activities.",
+  },
+  {
+    id: 2,
+    imgSrc: img2,
+    destTitle: "Machu Picchu",
+    location: "Peru",
+    grade: "CULTURAL RELAX",
+    fees: "$600",
+    description:
+      "Huayna Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the Sun Gate is simply spectacular.",
+  },
+  {
+    id: 3,
+    imgSrc: img3,
+    destTitle: "Great Barrier Reef",
+    location: "Australia",
+    grade: "CULTURAL RELAX",
+    fees: "$700",
+    description:
+      "One of the most remarkable Australian natural gifts is the Great Barrier Reef. The hallmark of this place is lavish and beauty. Always interesting to be in this place!",
+  },
+  {
+    id: 4,
+    imgSrc: img4,
+    destTitle: "Cappadocia",
+    location: "Turkey",
+    grade: "CULTURAL RELAX",
+    fees: "$800",
+    description:
+      "According to the World Tourism Ranking, 45 Million people visit Turkey each year, and from that about 2 Million come to visit Cappadocia. This place is known for its luxurious stays and adventurous activities.",
+  },
+  {
+    id: 5,
+    imgSrc: img5,
+    destTitle: "Guanajuato",
+    location: "Mexico",
+    grade: "CULTURAL RELAX",
+    fees: "$1100",
+    description:
+      "A city in central Mexico, Guanajuato is known for its history of silver mining and colonial architecture. The houses in the city are very attractively painted with the most bright colors available.",
+  },
+  {
+    id: 6,
+    imgSrc: img6,
+    destTitle: "Cinque Terre",
+    location: "Italy",
+    grade: "CULTURAL RELAX",
+    fees: "$840",
+    description:
+      "The vibrant hues of the houses are its benchmark and explain the beauty of the place. Also, if you are a foodie and love seafood, you will be exhilarated with the choice you are about to get here.",
+  },
+  {
+    id: 7,
+    imgSrc: img7,
+    destTitle: "Angkor Wat",
+    location: "Cambodia",
+    grade: "CULTURAL RELAX",
+    fees: "$790",
+    description:
+      "Angkor wat represents the entire range of khmer art from the 9th to the 15th century. This temple is considered the heart and soul of Cambodia. This place is known for its luxurious stays and adventurous activities",
+  },
+  {
+    id: 8,
+    imgSrc: img8,
+    destTitle: "Taj Mahal",
+    location: "India",
+    grade: "CULTURAL RELAX",
+    fees: "$900",
+    description:
+      "An immense mausoleum of white marble, built in Agra by Mughal emperor Shah Jahan in memory of his wife Mumtaz, the monument is breathtakingly beautiful. This palce is known for its luxurious stays and adventurous activities",
+  },
+  {
+    id: 9,
+    imgSrc: img9,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    grade: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesian Island and one of the best holiday destinations in the Indonesian archipelago. Bli is konwn for its volcanic mountains, history, art & culture, food, temples and beautiful sandy beaches",
+  },
+];
