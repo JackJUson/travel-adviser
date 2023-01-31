0e19145b49b5b843b16dfbfd8ea47be86ac588ea
Jackson <z5397791@ad.unsw.edu.au>
2023-01-31 18:54:37 +1100

    Add card div with destination, date and price input

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./home.css";
 import video from "../../assets/video.mp4";
+import { GrLocation } from "react-icons/gr";
 
 const Home = () => {
   return (
@@ -10,12 +11,33 @@ const Home = () => {
 
       <div className="homeContent container">
         <div className="textDiv">
-          <span className="smallText">
-            Out Packages
-          </span>
-          <h1 className="homeTitle">
-            Search your Holiday
-          </h1>
+          <span className="smallText">Out Packages</span>
+          <h1 className="homeTitle">Search your Holiday</h1>
+        </div>
+
+        <div className="cardDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">Search your destination:</label>
+            <div className="input flex">
+              <input type="text" placeholder="Enter city here..." />
+              <GrLocation className="icon" />
+            </div>
+          </div>
+          <div className="dateInput">
+            <label htmlFor="city">Select your date:</label>
+            <div className="input flex">
+              <input type="date" />
+            </div>
+          </div>
+          <div className="priceInput">
+            <div className="label_total flex">
+              <label htmlFor="price">Max price:</label>
+              <h3 className="total">$5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="1000" />
+            </div>
+          </div>
         </div>
       </div>
     </section>
