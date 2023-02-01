f5ea432315be886b4197a934728102201718a7b0
Jackson <z5397791@ad.unsw.edu.au>
2023-02-01 12:38:08 +1100

    Add footer card and footer links to footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./footer.css";
 import video2 from "../../assets/video2.mp4";
 import { FiSend } from "react-icons/fi";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { FaTripadvisor } from "react-icons/fa";
+import { FiChevronRight } from "react-icons/fi";
+import { AiOutlineTwitter, AiFillYoutube, AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
@@ -22,6 +26,56 @@ const Footer = () => {
             <button className="btn flex" type="submit">
               Send <FiSend className="icon" />
             </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className="logo flex">
+                <MdOutlineTravelExplore className="icon" /> Travel.
+              </a>
+            </div>
+
+            <div className="footerParagraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima veniam obcaecati magnam earum! Eveniet quas excepturi odit explicabo suscipit, omnis inventore fugiat, harum reiciendis libero cum reprehenderit repellendus rerum neque?
+            </div>
+
+            <div className="footerSocials">
+              <AiOutlineTwitter className="icon" />
+              <AiFillYoutube className="icon" />
+              <AiFillInstagram className="icon" />
+              <FaTripadvisor className="icon" />
+            </div>
+          </div>
+
+          <div className="footerLinks grid">
+            <div className="linkGroup">
+              <span className="groupTitle">
+                Out Agency
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Payment
+              </li>
+            </div>
           </div>
         </div>
       </div>
